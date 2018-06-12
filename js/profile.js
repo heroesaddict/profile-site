d03fb6d0e6261fb0076359363cebe2a8fbbbca5b
heroesaddict <chattokim@gmail.com>
2018-06-12 10:07:27 +0800

    Update profile.js

--- js/profile.js
+++ js/profile.js
@@ -5,16 +5,3 @@ $(document).ready(function(){
     $('.m_btn').click(function(){
     $('#nav').stop().slideToggle();
 });
-  // Add smooth scrolling to all links in navbar + footer link
-  $(".sidenav a").on('click', function(event) {
-    var hash = this.hash;
-    if (hash) {
-      event.preventDefault();
-      $('html, body').animate({
-        scrollTop: $(hash).offset().top
-      }, 900, function() {
-        window.location.hash = hash;
-      });
-    }
-
-  });
